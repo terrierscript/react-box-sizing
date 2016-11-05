9e00255eed01db28b9b34d6afa2703e5abb1e709
inuscript <inuscript@gmail.com>
2016-11-05 10:35:01 +0900

    Box sizing component

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react'
+import { style, merge, select} from 'glamor'
+
+const BoxSizing = ({type = 'border-box', children}) => {
+  return <div {...merge(
+    style({ boxSizing: type}),
+    select(' *', { boxSizing: type})
+    select(' *:after', { boxSizing: type})
+    select(' *:before', { boxSizing: type})
+  )}>
+    {children}
+  </div>
+}
+
+BoxSizing.propTypes = {
+  type: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
+
+export default BoxSizing
